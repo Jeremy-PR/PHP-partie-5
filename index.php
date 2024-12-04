4545a4a14c02eb64af966f25d1333560100eb4b7
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-12-04 16:31:36 +0100

    add : part 5 done

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+//  EXERCICE 1
+
+// $months = ['janvier' , 'fevrier' , 'mars' , 'avril' , 'mai' , 'juin' , 'juillet' , 'aout' ,'septembre' ,'octobre' ,'novembre' ,'décembre'];
+// var_dump($months)
+
+
+// EXERCICE 2
+
+// $months = ['janvier' , 'fevrier' , 'mars' , 'avril' , 'mai' , 'juin' , 'juillet' , 'aout' ,'septembre' ,'octobre' ,'novembre' ,'décembre'];
+
+// echo $months[2]
+
+
+// Exercice 3
+ 
+
+// $months = ['janvier' , 'fevrier' , 'mars' , 'avril' , 'mai' , 'juin' , 'juillet' , 'aout' ,'septembre' ,'octobre' ,'novembre' ,'décembre'];
+
+
+// echo $months[5]
+
+
+// EXERCICE 4
+
+// $months = ['janvier' , 'fevrier' , 'mars' , 'avril' , 'mai' , 'juin' , 'juillet' , 'aout' ,'septembre' ,'octobre' ,'novembre' ,'décembre'];
+
+// $months [7] = 'août';
+// echo $months[7];
+
+
+// Exercice 5
+
+// $tableau = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise',62 => 'PDC', 80 => 'Somme'];
+
+// 
+
+
+// Exercice 6
+
+// $tableau = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise',62 => 'PDC', 80 => 'Somme'];
+
+// echo $tableau[59];
+
+
+
+// Exercice 7
+
+// $tableau = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise',62 => 'PDC', 80 => 'Somme'];
+// $tableau[51] = 'Marne' ;
+// echo $tableau[51]
+
+
+// Exercice 8
+
+
+// $months = ['janvier' , 'fevrier' , 'mars' , 'avril' , 'mai' , 'juin' , 'juillet' , 'aout' ,'septembre' ,'octobre' ,'novembre' ,'décembre'];
+
+// foreach($months as $value){
+
+//     echo $value . "<br>" ;
+// }
+
+
+
+// Exercice 9
+
+
+// $tableau = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise', 62 => 'PDC', 80 => 'Somme'];
+
+// foreach($tableau as $value){
+//     echo $value . "<br>";
+// }
+
+
+
+
+// Exercice 10
+
+
+// $tableau = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise',62 => 'PDC', 80 => 'Somme'];
+
+// foreach($tableau as $key => $value){
+//         echo "Le numéro de département  " . $key . " correspond au département : " . $value . "<br>";
+// }
+?>
